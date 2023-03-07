2318f42ee43f5dbc0aa757d405b8fbbe1785250d
MoisesAldas <mmaldas1@espe.edu.ec>
2023-03-07 00:28:00 -0500

    CommitFinals

--- formularioadopciones.php
+++ formularioadopciones.php
@@ -510,7 +510,7 @@ $(document).ready(function () {
   });
 
   $("#Contactos").mouseover(function () {
-    <!--Cambiar el fondo de color negro con letra blanca-->
+
     $(this).css("background-color", "black");
     $(this).css("color", "white");
   });
