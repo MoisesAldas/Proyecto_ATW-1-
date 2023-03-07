7bc848cff1bf04ad61475798322d254cc8cce7a1
alejandrovf9 <fajardotoaquiza@outlook.es>
2023-03-06 21:50:01 -0500

    ContactoPhp

--- About_us.php
+++ About_us.php
@@ -152,7 +152,7 @@
                     </ul>
                     <form class="d-flex" role="search">
 
-                        <a href="contact.html"><button type="button" class="btn   fw-semibold myBtn" style="background-color:#b2e8f9 ;border-color: #151515;" id="Contactos">Contacto</button> </a>
+                        <a href="contacto.php"><button type="button" class="btn   fw-semibold myBtn" style="background-color:#b2e8f9 ;border-color: #151515;" id="Contactos">Contacto</button> </a>
                     </form>
                 </div>
 
@@ -185,7 +185,7 @@
             <hr>
             Acuda a los <cite title="Contactos">Contactos para responder dudas e inquietudes</cite>
             <br><br>
-            <a href="contact.html"><button type="button" class="btn fw-semibold" style="background-color:#b2e8f9;">Contacto</button></a>
+            <a href="contacto.php"><button type="button" class="btn fw-semibold" style="background-color:#b2e8f9;">Contacto</button></a>
           </div>
         </div>
       </div>
